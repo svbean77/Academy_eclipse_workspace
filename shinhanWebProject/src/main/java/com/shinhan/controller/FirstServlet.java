a07dd6c70400b548f43bf91fc69d3024bf604376
Eunbeen Oh <svbean77@naver.com>
2023-03-25 23:25:29 +0900

    day022

---
+++ shinhanWebProject/src/main/java/com/shinhan/controller/FirstServlet.java
@@ -0,0 +1,46 @@
+package com.shinhan.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/first") // 브라우저에서 이런 요청이 들어오면 FirstServlet이 실행되어라! 라고 tomcat이 알아서 해줌!
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FirstServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().append("<h1>나의 서버 Served at: ").append(request.getContextPath()).append("!!!!!!</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ shinhanWebProject/src/main/java/com/shinhan/controller/LoginServlet.java
@@ -0,0 +1,48 @@
+// 04 -> form의 정보를 서버에 보냄!
+package com.shinhan.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/day1/loginConfirm") // html의 경로가 day1/login이기 때문에 얘도 day1/로 만들어야 함@
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("userid"); // input 태그의 name 이름과 같아야 함! '그 정보'를 받아오겠다!
+		String pass = request.getParameter("userpass"); // getParameter는 무조건 문자열임
+
+		System.out.println("아이디: " + id + ", 비밀번호: " + pass);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
